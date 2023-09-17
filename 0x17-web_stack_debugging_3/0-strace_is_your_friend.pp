3399769353fa52d70909cbe00a625ae5ab060aea
Henry <83168293+Orjihenry@users.noreply.github.com>
2023-09-17 09:23:19 +0100

    Delete 0x17-web_stack_debugging_3 directory
    Deferred to a new chohort

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Bug fix on wordpress site running on apache2
-
-exec { 'wordpress bug fix':
-  path    => ['/bin', '/usr/bin', '/usr/sbin'],
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
-}
