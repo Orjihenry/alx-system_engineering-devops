32576ed77eef5c7e7adad7deba3b4ee7256fefcb
Orjihenry <orjihen@yahoo.com>
2023-10-18 13:23:03 +0100

    Sky is the limit, let's bring that limit higher ✅⚡💫

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# Increase the ULIMIT to 4096 to fix bug
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
